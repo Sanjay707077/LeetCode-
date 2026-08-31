d48f2572517e894ef49513f2f0c8972449f04118
Sanjay707077 <tsanjay7070@gmail.com>
2026-08-31 19:45:50 +0530

    Time: 4 ms (74.11%) | Memory: 44.8 MB (86.52%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -4,28 +4,30 @@ class Solution {
         Stack<Integer> stack = new Stack<Integer>(); 
         int[] result = new int[nums1.length];
 
-        for(int i = 0; i < nums1.length; i++) { 
-            for(int j = 0; j < nums2.length; j++) { 
-                
-                if(nums1[i] == nums2[j]) { 
-                    
+        for(int i = 0; i < nums1.length; i++) 
+        { 
+            for(int j = 0; j < nums2.length; j++) 
+            { 
+                if(nums1[i] == nums2[j]) 
+                { 
                     stack.push(-1);
 
-                    for(int k = j + 1; k < nums2.length; k++) {
-                        
-                        if(nums2[k] > nums2[j]) {
+                    for(int k = j + 1; k < nums2.length; k++)
+                    {
+                        if(nums2[j] < nums2[k])
+                        {
                             stack.pop();
                             stack.push(nums2[k]);
                             break;
                         }
                     }
-
                     break;
                 } 
             } 
-        }
-
-        for(int i = 0; i < nums1.length; i++) {
+        } 
+ 
+        for(int i = 0; i < nums1.length; i++) 
+        {
             result[i] = stack.get(i);
         }
 
